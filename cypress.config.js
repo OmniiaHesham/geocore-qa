b28fe9ed21556b3a36988a30ac52de4986f4b8d6
OmniiaHesham <ohesham@gt.com.sa>
2025-05-15 12:41:38 +0300

    upload data set step one

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,31 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  video: false,
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    reporterEnabled: 'cypress-qase-reporter',
+    cypressQaseReporterReporterOptions: {
+      mode: "testops",
+      debug: true,
+      testops: {
+        api: {
+          token: "6f6170e8d3436053690fc4f9323df9aa0b9514fa27d0110f0e4f2c7de44df1a0",
+        },
+        project: "GEOCORE",
+        uploadAttachments: true,
+        run: {
+          complete: true,
+        },
+      },
+      framework: {
+        cypress: {
+          screenshotsFolder: 'cypress/screenshots',
+        }
+      }
+    }
+  },
+
   e2e: {
     baseUrl: "https://geocore.dev.geotech.build/",
     viewportWidth: 1440,
@@ -11,16 +36,10 @@ module.exports = defineConfig({
     requestTimeout: 30000,
     includeShadowDom: true,
     ensureScrollable: false,
-    reporter: "cypress-qase-reporter",
-    reporterOptions: {
-      apiToken: "6f6170e8d3436053690fc4f9323df9aa0b9514fa27d0110f0e4f2c7de44df1a0",
-      projectCode: "GEO",
-      logging: true,
-      debug: true,
-    },
+
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-qase-reporter/plugin')(on, config);
+      require('cypress-qase-reporter/metadata')(on);
     },
   },
 });
-
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -4,6 +4,8 @@ Cypress.on('uncaught:exception', (err, runnable) => {
     return false;
   });
   
+  import 'cypress-file-upload';
+
 
 
 // ***********************************************************
